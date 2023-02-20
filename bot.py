16234d91514942ec2f5fe1bedb2aa55c2c020c82
Mikhail Zotov <mik.and.zotov@gmail.com>
2023-02-21 00:50:35 +0300

    простейший бот готов!

--- bot.py
+++ bot.py
@@ -1,1 +1,24 @@
-print('Gvido')
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from config import Variables
+
+
+def on_start_command(update, context):
+    print('Start event message received!')
+    print(f'update: {update}')
+    update.message.reply_text(f'Hi, {update.message.chat.username}!')
+
+
+def main():
+    bot = Updater(token=Variables.token)
+    dp = bot.dispatcher
+    dp.add_handler(CommandHandler('start', on_start_command))
+    dp.add_handler(CommandHandler('begin', on_start_command))
+
+    bot.start_polling()
+    bot.idle()
+
+
+if __name__ == '__main__':
+    main()
+
